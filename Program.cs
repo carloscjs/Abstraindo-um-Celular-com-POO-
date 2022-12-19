4a31a76c4d58a525f8c5a496dd69f778dc34cb5f
carloscjs <carlos_silvatio@yahoo.com.br>
2022-12-19 00:34:02 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using DesafioPOO.Models;
+
+
+Console.WriteLine("Teste do nokia");
+Smartphone nokia = new Nokia(numero: "95877-8874", modelo: "A22", imei: "ABC2335424", memoria: 124);
+nokia.Ligar();
+nokia.InstalarAplicativo("facebook");
+
+
+Console.WriteLine("Teste do Iphone");
+Smartphone iphone = new Iphone(numero: "95622-6798", modelo: "8s", imei: "ADC1213412", memoria: 248);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("twiter");
+
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
